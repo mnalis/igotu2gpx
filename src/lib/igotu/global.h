fb71538ec9e9e9679e81a52b18b420024661d7bf
Matija Nalis <mnalis-git@voyager.hr>
2017-08-03 16:30:44 +0200

    bump version to 0.4.0.227.17 when building .deb
    import branches from bzr with https://github.com/FluidityProject/fluidity/wiki/Migrating-branches-from-bzr

--- src/lib/igotu/global.h
+++ src/lib/igotu/global.h
@@ -21,7 +21,7 @@
 
 #include <QtGlobal>
 
-#define IGOTU_VERSION_STR "0.4.0.227.16"
+#define IGOTU_VERSION_STR "0.4.0.227.17"
 
 #if defined(Q_CC_MSVC)
     #define DECLARE_DEPRECATED __declspec(deprecated)
