11ccbb16dbdd141cd633b60bb4518d1de46affe3
Matija Nalis <mnalis-git@voyager.hr>
2017-08-03 12:25:37 +0200

    debian build initial version

--- tools/debian-snapshots.sh
+++ tools/debian-snapshots.sh
@@ -2,50 +2,18 @@
 
 [ -d src ] || exit 1
 
-LASTVERSION=`sed -n 's/igotu2gpx (\([^-~]*\).*/\1/p;T;q' debian/changelog`
-VERSION=`echo $LASTVERSION | sed 's/^\([0-9]*\.[0-9]*\).*$/\1.90/'`
-DATE=`date "+%Y%m%d%H%M"`
-FULLVERSION=$VERSION+bzr$DATE
-
-[ -z "`bzr status`" ] || {
-    echo "Uncommitted changes, aborting" >&2
-    exit 1
-}
+LASTVERSION=`sed -ne 's/^.*IGOTU_VERSION_STR "\(.*\)"/\1/p' src/lib/igotu/global.h`
+FULLVERSION=`sed -n 's/igotu2gpx (\([^-~ )]*\).*/\1/p;T;q' debian/changelog`
 
 echo "last version: $LASTVERSION"
 echo "new version: $FULLVERSION (temporary)"
 
-rm -f ../build-area/igotu2gpx_$FULLVERSION.orig.tar.gz
-bzr revert
+[ -z "`git status --short`" ] || {
+    echo "Uncommitted changes, aborting" >&2
+    exit 1
+}
 
-MSGFILE=$(tempfile) || exit
-trap "rm -f -- '$MSGFILE'" EXIT
-echo "  * Changes since version $LASTVERSION:" >> "$MSGFILE"
-COLUMNS=1000 bzr log --line -r tag:igotu2gpx-$LASTVERSION..-1 | head -n -1 | sed 's/^.*[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\} /    - /' >> "$MSGFILE"
+sed -i 's/\(#define \{1,\}IGOTU_VERSION_STR \).*/\1'"\"$FULLVERSION\"/" src/lib/igotu/global.h
+git commit --all --message="bump version to $FULLVERSION when building .deb" --edit
 
-vi "$MSGFILE"
-
-TARBALL=-sa
-for dist in intrepid jaunty karmic; do
-    dch -v $FULLVERSION-1~${dist}1 -D $dist "YYYTEMPYYY"
-    (
-	cat debian/changelog | sed -n '0,/YYYTEMPYYY/p'
-	echo "  * Released $dist bzr snapshot."
-	cat "$MSGFILE"
-	cat debian/changelog | sed -n '/YYYTEMPYYY/,$p'
-    ) | grep -v YYYTEMPYYY | sponge debian/changelog
-
-    sed -i 's/\(#define \{1,\}IGOTU_VERSION_STR \).*/\1'"\"$FULLVERSION\"/" src/igotu/global.h
-
-    bzr builddeb --builder "debuild $TARBALL -S -pgnome-gpg -sgpg" || true
-    bzr revert
-    TARBALL=-sd
-done
-
-rm -f -- "$MSGFILE"
-trap - EXIT
-
-echo "Really publish to launchpad?"
-read
-
-dput ppa:igotu2gpx/rc-archive ../build-area/igotu2gpx_$FULLVERSION-1~*_source.changes
+debuild -sa
